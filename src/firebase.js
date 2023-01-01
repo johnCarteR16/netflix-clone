1946e7a2e1ab42152a966405fa78fdcd5a4bfca1
johnCarteR16 <nlsnrjlinux1141@gmail.com>
2023-01-01 21:47:55 +0530

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwdFFOXqG_RHJioSFlhNGgxahDHV9O37I",
+    authDomain: "netflix-clone-229d7.firebaseapp.com",
+    projectId: "netflix-clone-229d7",
+    storageBucket: "netflix-clone-229d7.appspot.com",
+    messagingSenderId: "1042454872418",
+    appId: "1:1042454872418:web:0dccaa66c6e29c25df6c3b"
+  };
