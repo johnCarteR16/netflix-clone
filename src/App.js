6ac86c77dd69523ef89313a60ae577dcda939fdd
johnCarteR16 <nlsnrjlinux1141@gmail.com>
2023-01-01 23:29:48 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { HomeScreen } from './components/HomeScreen';
+import './components/HomeScreen.css'
 
 function App() {
     return (
       <div className="app">
-        <h1>Hello World</h1>
+        <HomeScreen />
       </div>
     );
 }
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav } from './Nav'
+
+export const HomeScreen = () => {
+    return (
+        <div className='homeScreen'>
+            <Nav />
+            
+        </div>
+    )
+}
